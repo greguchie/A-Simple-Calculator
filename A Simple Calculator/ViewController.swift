e44f3b60c745dadcc59f6932f4be0d643c0192de
Greg <greguchieokoro@icloud.com>
2026-02-28 08:46:07 -0500

    Initial Commit

---
+++ A Simple Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A Simple Calculator
+//
+//  Created by Kensei on 2026-02-28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
