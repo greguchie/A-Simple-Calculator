ea056ef07d964a6637c13a7d370e073dd5ba67bb
Greg <greguchieokoro@icloud.com>
2026-02-28 10:56:29 -0500

    Link buttons to controller file

--- A Simple Calculator/ViewController.swift
+++ A Simple Calculator/ViewController.swift
@@ -14,5 +14,35 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    private var isFinishTyping: Bool = true
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        isFinishTyping = true
+        
+        guard let number = Double(displayLabel.text!)
+        else {
+            fatalError("Cannot convert display label text to Double.")
+        }
+        
+        // when a non-number is pressed
+    }
+    
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        if let numValue = sender.titleLabel?.text {
+            if isFinishTyping {
+                displayLabel.text = numValue
+                isFinishTyping = false
+            }
+            else {
+                displayLabel.text = displayLabel.text! + numValue
+            }
+            
+        }
+    }
+    
+    
 }
 
